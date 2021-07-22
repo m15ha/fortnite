fac87e6192184fc69e8ee9fba129f5e8b3329f7d
Misha <mihail.lazareff@gmail.com>
2021-07-22 13:14:13 -0400

    2021-07-22 end work

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,24 @@
-import React from "react";
+import React from 'react';
 
 export default function Header() {
-  return (
-    <nav className="light-blue darken-1">
-      <div className="nav-wrapper">
-        <a href="!#" className="brand-logo">
-        Fortnite shop 
-        </a>
-        <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li>
-            <a href="!#">Home</a>
-          </li>
-          <li>
-            <a href="!#">About</a>
-          </li>
-          <li>
-            <a href="!#">Repo</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className='light-blue darken-1'>
+            <div className='nav-wrapper'>
+                <a href='!#' className='brand-logo'>
+                    Fortnite shop
+                </a>
+                <ul id='nav-mobile' className='right hide-on-med-and-down'>
+                    <li>
+                        <a href='!#'>Home</a>
+                    </li>
+                    <li>
+                        <a href='!#'>About</a>
+                    </li>
+                    <li>
+                        <a href='https://github.com/m15ha/fortnite'>Repo</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
-const API_KEY = process.env.REACT_APP_API_KEY;
+// const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = '7a0824c4-fb9201ed-98286fc0-5f9c6ddd';
 
-const API_URL = 'https://fortniteapi.io/v1/shop?lang=ru';
+const API_URL = 'https://fortniteapi.io/v1/shop?lang=en';
 
 export { API_KEY, API_URL };
