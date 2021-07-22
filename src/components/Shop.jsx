ac3da75f780fd7126e41d4a107253288b1dedbc0
Misha <mihail.lazareff@gmail.com>
2021-07-22 12:51:18 -0400

    shop

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { API_KEY, API_URL } from '../config';
+import Preloader from './Preloader';
+import GoodsList from './GoodsList';
 
 export default function Shop() {
+    const [goods, setGoods] = useState([]);
+    const [loading, setLoading] = useState([]);
+
+    useEffect(function getGoods() {
+        fetch(API_URL, {
+            headers: {
+                Authorization: API_KEY,
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                data.featured && setGoods(data.featured);
+                setLoading(false);
+            });
+    }, []);
+
     return (
         <>
-           <main className="container content">
-               Test msg
-               </main> 
+            <main className='container content'>
+                {loading ? <Preloader /> : <GoodsList goods={goods} />}
+            </main>
         </>
-    )
+    );
 }
