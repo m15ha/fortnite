5af08d1e752229f64a8fa8bfdffeb5a0402335cd
Misha <mihail.lazareff@gmail.com>
2021-07-22 15:47:39 -0400

    test

---
+++ src/components/GoodsList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import GoodsItem from './GoodsItem'
+
+export default function GoodsList(props) {
+    const {goods = []} = props;
+    if (!goods.length){
+        return <h3>Nothing here</h3>
+    }
+    return (
+        <div className='goods'>
+            {goods.map ((item) => (<GoodsItem key={item.id} {...item} />))}
+        </div>
+    )
+}
